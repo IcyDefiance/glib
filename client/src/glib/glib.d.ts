f09bb1f620652607b37f48965eaeba17c2dc7bd5
IcyDefiance <icydefiance@gmail.com>
2019-11-16 21:45:21 -0500

    Change typings

--- client/src/glib/glib.d.ts
+++
@@ -1,2 +0,0 @@
-declare const dummy: any;
-export default dummy;
--- client/src/state/objects.ts
+++ client/src/state/objects.ts
@@ -1,11 +1,11 @@
 import { Observable, Subject } from "rxjs";
 import { filter } from "rxjs/operators";
-import { PackObject, UnpackObject } from "../glib/glib";
+import * as Glib from "../glib/glib.js";
 import { AutoID, Message, NetworkObject, ObjectType } from "../glib/layouts";
 
 let evtSource = new EventSource("/api/stream");
 evtSource.addEventListener("message", event =>
-	objsS.next(UnpackObject(JSON.parse((event as MessageEvent).data)[0].data)),
+	objsS.next(Glib.UnpackObject(JSON.parse((event as MessageEvent).data)[0].data)),
 );
 
 const objsS = new Subject<NetworkObject>();
@@ -16,7 +16,7 @@ objs$.subscribe(obj => console.log("stream", obj));
 
 export async function publish(obj: NetworkObject) {
 	console.log("publish", obj);
-	const body = PackObject(obj);
+	const body = Glib.PackObject(obj);
 	const headers = { "Content-Type": "application/octet-stream" };
 	const res = await fetch("/api/publish", { method: "POST", body, headers });
 	return res.json();
