3fe25cc1f912a990c0e048a96604bd8c3fb8835a
IcyDefiance <icydefiance@gmail.com>
2019-11-16 20:49:19 -0500

    Pack objects

--- client/src/state/objects.ts
+++ client/src/state/objects.ts
@@ -1,24 +1,24 @@
 import { Observable, Subject } from "rxjs";
 import { filter } from "rxjs/operators";
+import { PackObject, UnpackObject } from "../glib/glib";
 import { AutoID, Message, NetworkObject, ObjectType } from "../glib/layouts";
 
 let evtSource = new EventSource("/api/stream");
-evtSource.addEventListener("object-data", event => {
-	const ev = event as MessageEvent;
-	objsS.next(JSON.parse(JSON.parse(ev.data).data));
-	console.log(ev);
-});
+evtSource.addEventListener("message", event =>
+	objsS.next(UnpackObject(JSON.parse((event as MessageEvent).data)[0].data)),
+);
 
 const objsS = new Subject<NetworkObject>();
 export const objs$ = objsS.asObservable();
 export const messages$ = objs$.pipe(filter(obj => obj.type == ObjectType.Message)) as Observable<Message>;
 
+objs$.subscribe(obj => console.log("stream", obj));
+
 export async function publish(obj: NetworkObject) {
-	const res = await fetch("/api/publish", {
-		method: "POST",
-		body: JSON.stringify(obj),
-		headers: { "Content-Type": "application/json" },
-	});
+	console.log("publish", obj);
+	const body = PackObject(obj);
+	const headers = { "Content-Type": "application/octet-stream" };
+	const res = await fetch("/api/publish", { method: "POST", body, headers });
 	return res.json();
 }
 
