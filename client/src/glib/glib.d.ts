c29766abf6c8ba0f747931f334964de95abe220b
IcyDefiance <icydefiance@gmail.com>
2019-11-16 21:13:10 -0500

    Type glib.js as any

--- client/src/glib/glib.d.ts
+++ client/src/glib/glib.d.ts
@@ -1,8 +1,2 @@
-export const Host: any;
-export const User: any;
-export const PackObject: any;
-export const UnpackObject: any;
-export const AccountKeygen: any;
-export const DeviceKeygen: any;
-export const OnLoad: any;
-export const VM: any;
+declare const dummy: any;
+export default dummy;
